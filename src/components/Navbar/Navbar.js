38380107a9ffd1eb9faa5416dcdf8ebd612c0ae8
Darolo13 <70618345+Darolo13@users.noreply.github.com>
2022-02-11 17:44:23 -0700

    Render Navbar component

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Navbar() {
+  return (
+    <div className="navbar">
+      <a href="/about"> ABOUT</a>
+      <a href="/workouts"> WORKOUTS</a>
+      <a href="/nutrition"> NUTRITION</a>
+      <a href="/stories"> STORIES</a>
+      <a href="/contact"> CONTACT</a>
+    </div>
+  );
+}
+
+export default Navbar;
