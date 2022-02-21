93d00642974d1ff3d5d8bef8f89757cefd305c69
Darolo13 <70618345+Darolo13@users.noreply.github.com>
2022-02-21 00:59:00 -0700

    initial navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
+// general
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+// style
+import './App.css';
+
+// pages
+import Home from './pages';
+
+// components
 import Navbar from './components/Navbar';
+import About from './pages/About';
+import Workouts from './pages/Workouts';
+import Contact from './pages/Contact';
+import Nutrition from './pages/Nutrition';
+import Stories from './pages/Stories';
 
 function App() {
   return (
-    <div className="App">
+    <BrowserRouter>
       <Navbar />
-    </div>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/workouts' element={<Workouts />} />
+        <Route path='/nutrition' element={<Nutrition />} />
+        <Route path='/stories' element={<Stories />} />
+        <Route path='/contact-us' element={<Contact />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
